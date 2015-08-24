980557cb7a128dfbf34094081b81c947228b11fd
Maria Yu <aiquny@codeaurora.org>
2015-08-24 18:04:04 +0800

    android: binder: add rate limit for possible excessive log
    Got a device crash when there is exessive log of below:
    "binder_alloc_buf failed to map pages in userspace, no vma".
    So add printk rate limit for fix this.
    
    Change-Id: If681cef95e3fee67dd88db5a86925b18d07a4ff9
    Signed-off-by: Maria Yu <aiquny@codeaurora.org>

--- drivers/android/binder.c
+++ drivers/android/binder.c
@@ -39,6 +39,7 @@
 #include <linux/slab.h>
 #include <linux/pid_namespace.h>
 #include <linux/security.h>
+#include <linux/ratelimit.h>
 
 #ifdef CONFIG_ANDROID_BINDER_IPC_32BIT
 #define BINDER_IPC_32BIT 1
@@ -686,7 +687,7 @@ static int binder_update_page_range(struct binder_proc *proc, int allocate,
 		goto free_range;
 
 	if (vma == NULL) {
-		pr_err("%d: binder_alloc_buf failed to map pages in userspace, no vma\n",
+		pr_err_ratelimited("%d: binder_alloc_buf failed to map pages in userspace, no vma\n",
 			proc->pid);
 		goto err_no_vma;
 	}
