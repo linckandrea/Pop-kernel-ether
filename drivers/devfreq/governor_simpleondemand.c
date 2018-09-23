a0af5c3da2d6d67930a4f7192aa1a627315dd8b5
linckandrea <linckandrea01@gmail.com>
2018-09-23 11:35:03 +0200

    GPU: governor_simpleondemand: optimize for 27mhz in idle

--- drivers/devfreq/governor_simpleondemand.c
+++ drivers/devfreq/governor_simpleondemand.c
@@ -74,8 +74,11 @@ static int devfreq_simple_ondemand_func(struct devfreq *df,
 
 	/* Set MAX if it's busy enough */
 	if (stat.busy_time * 100 >
-	    stat.total_time * dfso_upthreshold) {
-		*freq = max;
+	        stat.total_time * dfso_upthreshold) {
+		if (stat.current_frequency >= 180000000)
+			*freq = max;
+		else
+			*freq = 180000000;
 		return 0;
 	}
 
