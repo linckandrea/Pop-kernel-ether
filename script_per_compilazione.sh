3d2e5f4ead44ded7828874a91f7da817bc5da020
linckandrea <linckandrea01@gmail.com>
2018-09-23 18:04:23 +0200

    compiler-script: do make clean && make mrproper before starp compiling

--- script_per_compilazione.sh
+++ script_per_compilazione.sh
@@ -1,5 +1,7 @@
 export ARCH=arm64 && export SUBARCH=arm64
 
+make clean && make mrproper
+
 export CROSS_COMPILE=/home/andrea/aarch64-linux-gnu/bin/aarch64-linux-gnu-
 
 make lineageos_ether_defconfig
