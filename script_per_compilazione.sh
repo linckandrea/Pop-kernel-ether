16ea019d1f21ef19520ab632d1c589a0bb8d13f2
linckandrea <linckandrea01@gmail.com>
2018-10-14 13:50:13 +0200

    script: use linaro-opt-toolchain

--- script_per_compilazione.sh
+++ script_per_compilazione.sh
@@ -1,8 +1,110 @@
 export ARCH=arm64 && export SUBARCH=arm64
-
 make clean && make mrproper
+export CROSS_COMPILE=/home/andrea/aarch64-linux-android/bin/aarch64-opt-linux-android-
+make lineageos_ether_defconfig
+make -j4
 
-export CROSS_COMPILE=/home/andrea/aarch64-linux-gnu/bin/aarch64-linux-gnu-
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+android/bin/aarch64-opt-linux-android-
 
 make lineageos_ether_defconfig
 
