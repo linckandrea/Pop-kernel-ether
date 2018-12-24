145ecbf5c29e95ea79c6bd6120140d479f8ed40d
linckandrea <linckandrea01@gmail.com>
2018-12-24 12:52:09 +0100

    buil-script: do a make clean && make mrproper before new compile

--- script_per_compilazione.sh
+++ script_per_compilazione.sh
@@ -2,6 +2,8 @@ export ARCH=arm64 && export SUBARCH=arm64
 
 export CROSS_COMPILE=/home/andrea/aarch64-linux-android/bin/aarch64-opt-linux-android-
 
+make clean && make mrproper
+
 make lineageos_ether_defconfig
 
 make -j4
