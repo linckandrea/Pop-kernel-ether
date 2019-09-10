2ceff3e61d54e9f2d51b0a81f60d35777cd3be88
linckandrea <linckandrea@outlook.com>
2019-09-10 10:24:01 +0200

    Squash: P-r4 is here
     * also chage compiler and fix relative warnings

--- script_per_compilazione.sh
+++ script_per_compilazione.sh
@@ -1,5 +1,5 @@
 export ARCH=arm64 && export SUBARCH=arm64
 make clean && make mrproper
-export CROSS_COMPILE=/home/andrea/aarch64-linux-android/bin/aarch64-opt-linux-android-
+export CROSS_COMPILE=/home/andrea/aarch64-opt-linux-android/bin/aarch64-opt-linux-android-
 make lineageos_ether_defconfig
 make -j4
